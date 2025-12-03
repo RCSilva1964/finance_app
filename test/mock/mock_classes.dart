5ca71514875773ea44f17822dda53d5216fbd053
RCSilva1964 <robertocostaesilva@hotmail.com>
2025-12-03 08:24:26 -0300

    EP28: Introdução aos testes com Mocktail.

---
+++ test/mock/mock_classes.dart
@@ -0,0 +1,4 @@
+import 'package:finance_app/service/auth_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFirebaseAuthService extends Mock implements AuthService {}
---
+++ test/services/firebase_auth_service_test.dart
@@ -0,0 +1,58 @@
+import 'package:finance_app/common/models/user_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../mock/mock_classes.dart';
+
+void main() {
+  late MockFirebaseAuthService mockFirebaseAuthService;
+  late UserModel user;
+
+  setUp(() {
+    mockFirebaseAuthService = MockFirebaseAuthService();
+    user = UserModel(name: 'User', email: 'user@email.com', id: '1a2b3c4d5e');
+  });
+
+  test('Test Sign Up Success', () async {
+    final user = UserModel(
+      name: 'User',
+      email: 'user@email.com',
+      id: '1a2b3c4d5e',
+    );
+    when(
+      () => mockFirebaseAuthService.signUp(
+        name: 'User',
+        email: 'user@email.com',
+        password: 'user@123',
+      ),
+    ).thenAnswer((_) async => user);
+
+    final result = await mockFirebaseAuthService.signUp(
+      name: 'User',
+      email: 'user@email.com',
+      password: 'user@123',
+    );
+
+    expect(result, user);
+  });
+
+  test('Test Sign Up Failure', () async {
+    when(
+      () => mockFirebaseAuthService.signUp(
+        name: 'User',
+        email: 'user@email.com',
+        password: 'user@123',
+      ),
+    ).thenThrow(Exception());
+
+    expect(
+      () => mockFirebaseAuthService.signUp(
+        name: 'User',
+        email: 'user@email.com',
+        password: 'user@123',
+      ),
+      // throwsA(isInstanceOf<Exception>()),
+      throwsException,
+    );
+  });
+}
